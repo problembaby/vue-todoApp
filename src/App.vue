875e421dbce0e28651ab502ded038396c350d2b5
oh <cjdduf3@naver.com>
2023-02-09 21:48:56 +0900

    TodoHeader add contents

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,21 @@
 <!-- App.vue -->
+ 
 <template>
+   <div id="app">
+     <TodoHeader />
 
- <div id="app">
-    <TodoHeader></TodoHeader>
-    <TodoTitle></TodoTitle>
-    <TodoInput></TodoInput>
-    <TodoControllorer></TodoControllorer>
-    <TodoList></TodoList>
-    <TodoFooter></TodoFooter>
- </div >
+   </div>
+ </template>
+  
+ <script>
+ import TodoHeader from "./components/TodoHeader";
 
-</template>
+  
+ export default {
+   name: "App",
+   components: {
+     TodoHeader,
 
-<script>
-import TodoControllorer from './components/TodoControllorer.vue';
-import TodoFooter from './components/TodoFooter.vue';
-import TodoHeader from './components/TodoHeader.vue';
-import TodoInput from './components/TodoInput.vue';
-import TodoList from './components/TodoList.vue';
-import TodoTitle from './components/TodoTitle.vue';
-
-export default {
- name:'App',
- components:{
-  TodoControllorer,
-  TodoFooter,
-  TodoHeader,
-  TodoInput,
-  TodoList,
-  TodoTitle,
- }
-}
-</script>
-
-<style>
-
-</style>
+   }
+ };
+ </script>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+    <h1 class="logo">vueTodo</h1>
+    <p class="date">{{ timestamp }}</p>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      timestamp:""
+    }
+  },
+  created(){
+    const now = new Date();
+    const month = now.getMonth() + 1;
+    const date = now.getDate();
+    const weekList = new Array("sun","mon","tue","wed","thu","fri","sat");
+    const week = weekList[now.getDay()];
+    this.timestamp = `${month}/${date}.${week}`
+
+  },
+
+};
+</script>
+
+<style>
+
+</style>
