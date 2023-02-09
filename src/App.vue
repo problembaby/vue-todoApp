3a23bf309934612531da11d00a5a62ed0d6ebe5e
oh <cjdduf3@naver.com>
2023-02-09 15:52:53 +0900

    creating a Componet for the First

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
+<!-- App.vue -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+ <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoTitle></TodoTitle>
+    <TodoInput></TodoInput>
+    <TodoControllorer></TodoControllorer>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+ </div >
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoControllorer from './components/TodoControllorer.vue';
+import TodoFooter from './components/TodoFooter.vue';
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoTitle from './components/TodoTitle.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+ name:'App',
+ components:{
+  TodoControllorer,
+  TodoFooter,
+  TodoHeader,
+  TodoInput,
+  TodoList,
+  TodoTitle,
+ }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
