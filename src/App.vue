0a4038e830e62e506e90d0c6b0693a67252a4a31
oh <cjdduf3@naver.com>
2023-02-13 14:33:13 +0900

    Todo add style

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,11 @@
   
  <script>
  import TodoHeader from "./components/TodoHeader";
- import TodoTitle from "./components/TodoTitle.vue";
- import TodoList from "./components/TodoList.vue";
- import TodoInput from "./components/TodoInput.vue";
- import TodoControllorer from "./components/TodoControllorer.vue";
- import TodoFooter from "./components/TodoFooter.vue";
+ import TodoTitle from "./components/TodoTitle";
+ import TodoList from "./components/TodoList";
+ import TodoInput from "./components/TodoInput";
+ import TodoControllorer from "./components/TodoControllorer";
+ import TodoFooter from "./components/TodoFooter";
   
  export default {
    name: "App",
@@ -33,3 +33,20 @@
    }
  }
  </script>
+
+	
+<style lang="scss">
+//@import "./assets/style/_reset";
+
+.top {
+  width: 100%;
+  min-height: 43.6rem;
+  padding: 0 $padding 4.5rem;
+  background-image: linear-gradient(145deg, #3770cc 20%, #ce91c9 84%);
+}
+
+.body {
+  padding: 3rem $padding;
+  background-color: #efefef;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+
+module.exports = {
+  css: {
+    sourceMap: true,
+    loaderOptions: {
+      scss: {
+        prependData: `
+          @import "~@/assets/style/_mixins.scss";
+          @import "~@/assets/style/_variables.scss";
+          @import "~@/assets/style/_mediaQueries.scss";
+          @import "~@/assets/style/_svg.scss";
+          @import "~@/assets/style/_common.scss";
+        `
+      }
+    }
+  },
+  publicPath: '/vue-mytodo'
+}
