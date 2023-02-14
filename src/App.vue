a9afed384e88d8ad1a204a573638e256ded63ddd
oh <cjdduf3@naver.com>
2023-02-14 16:27:26 +0900

    troubleshooting scss

--- src/App.vue
+++ src/App.vue
@@ -35,18 +35,16 @@
  </script>
 
 	
+	
 <style lang="scss">
-//@import "./assets/style/_reset";
+@import "./assets/style/_reset";
 
-.top {
-  width: 100%;
-  min-height: 43.6rem;
-  padding: 0 $padding 4.5rem;
-  background-image: linear-gradient(145deg, #3770cc 20%, #ce91c9 84%);
-}
-
-.body {
-  padding: 3rem $padding;
-  background-color: #efefef;
+$color: #f0f0f0;
+.list {
+  background: $color;
+ 
+  &__text {
+    font-weight: bold;
+  }
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,13 @@
-
 module.exports = {
   css: {
-    sourceMap: true,
+    sourceMap:true,
     loaderOptions: {
-      scss: {
+      sass: {
         prependData: `
-          @import "~@/assets/style/_mixins.scss";
-          @import "~@/assets/style/_variables.scss";
-          @import "~@/assets/style/_mediaQueries.scss";
-          @import "~@/assets/style/_svg.scss";
-          @import "~@/assets/style/_common.scss";
+          @import "@/assets/style/_variables.scss";
+          @import "@/assets/style/_mixins.scss";
         `
       }
     }
-  },
-  publicPath: '/vue-mytodo'
-}
+  }
+};
